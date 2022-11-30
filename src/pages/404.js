a2574fb9e0a0f67c348d6b03f5295fa77cc606ac
liadensam <li.adensam@gmail.com>
2022-11-30 11:56:08 +0000

    updated node

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,30 +1,13 @@
-import * as React from "react"
-import { graphql } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
-const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-
+const Page = () => {
   return (
-    <Layout location={location} title={siteTitle}>
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
-  )
-}
+    <main>
+      <h1>Not Found Page</h1>
+      <Link to="/">Back</Link>
+    </main>
+  );
+};
 
-export const Head = () => <Seo title="404: Not Found" />
-
-export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
+export default Page;
---
+++ src/pages/{contentfulPage.url}.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+
+const Page = ({ data }) => {
+  return (
+    <main style={{ marginLeft: 20, marginTop: 20 }}>
+      <h1>{data.contentfulPage.title}</h1>
+      <p>{data.contentfulPage.description.description}</p>
+      <img
+        src={data.contentfulPage.image.file.url}
+        alt={data.title}
+        style={{
+          width: 300,
+          height: "auto",
+        }}
+      />
+      <Link style={{ marginRight: 20}} to="/">Go to Coffee time</Link>
+      <Link to="/about">Go to About Page</Link>
+    </main>
+  )
+}
+
+export const data = graphql`
+  query pageQuery($id: String) {
+    contentfulPage(id: { eq: $id }) {
+      url
+      title
+      description {
+        description
+      }
+      image {
+        file {
+          url
+        }
+      }
+    }
+  }
+`
+
+export default Page
